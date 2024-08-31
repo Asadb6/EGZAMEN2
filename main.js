355d62aad03fea6cf2f48d83942b8add59687915
Asadbek <Asadbek092009@gmail.com>
2024-08-31 21:24:29 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,15 @@
+
+const data = {
+    title: "Vibrant Portraits of 2020",
+    description:
+        "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.",
+    year: "2018",
+    name: "Illustration",
+    img: "https://shorturl.at/oKOzy",
+};
+
+document.getElementById("title").textContent = data.title;
+document.getElementById("description").textContent = data.description;
+document.getElementById("year").textContent = Year; {data.year};
+document.getElementById("name").textContent = Category; {data.name};
+document.getElementById("image").src = data.img;
